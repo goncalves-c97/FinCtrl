577142881755b36fb774c8768c71098d3ff1d1ea
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-01-23 19:50:17 -0300

    Update LogService

--- FinCtrl/FinCtrlLibrary/Services/LogService.cs
+++ FinCtrl/FinCtrlLibrary/Services/LogService.cs
@@ -1,27 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Serilog;
+using Serilog.Events;
 
 namespace FinCtrlLibrary.Services
 {
-    public class LogService
+    public static class LogService
     {
-        public static void AddLog(string context, string message)
+        public static void AddLog(string context, string message, LogEventLevel logEventLevel = LogEventLevel.Information)
         {
-            string logRegister = $"{context} - {message}";
-            Log.Information(logRegister);
+            string logInfo = $"{context} - {message}";
+            RegisterLog(logEventLevel, logInfo);
         }
 
-        public static void AddLog(string context, string message, object obj)
+        public static void AddLog(string context, string message, object obj, LogEventLevel logEventLevel = LogEventLevel.Information)
         {
-            string logRegister = $"{context} - {message}";
-            logRegister += JsonConvert.SerializeObject(obj, Formatting.Indented);
-            Log.Information(logRegister);
+            string logInfo = $"{context} - {message}";
+            logInfo += JsonConvert.SerializeObject(obj);
+            RegisterLog(logEventLevel, logInfo);
         }
 
-        public static void AddLog(string context, string message, string obj)
+        public static void AddLog(string context, string message, string obj, LogEventLevel logEventLevel = LogEventLevel.Information)
         {
-            string logRegister = $"{context} - {message}";
+            string logInfo = $"{context} - {message}";
 
             if (!string.IsNullOrEmpty(obj))
             {
@@ -32,29 +33,56 @@ namespace FinCtrlLibrary.Services
                 {
                     // Parse the JSON string
                     JsonConvert.DeserializeObject<object>(obj);
-                    logRegister += Environment.NewLine + obj;
+                    logInfo += Environment.NewLine + obj;
                 }
                 catch (JsonReaderException)
                 {
-                    // Invalid JSON
+                    RegisterLog(LogEventLevel.Error, $"{logInfo} - falha na desserialização do json - JsonReaderException");
+                    return;
                 }
                 catch (Exception)
                 {
-                    // Other exceptions (e.g., null, etc.)
+                    RegisterLog(LogEventLevel.Error, $"{logInfo} - falha na desserialização do json - Exception");
+                    return;
                 }
             }
 
-            Log.Information(logRegister);
+            RegisterLog(logEventLevel, logInfo);
         }
 
-        public static void AddLog(string guid, HttpRequest request, string body)
+        public static void AddLog(string guid, HttpRequest request, string body, LogEventLevel logEventLevel = LogEventLevel.Information)
         {
-            AddLog($"{request.Method} - {guid}", request.Path, body);
+            AddLog($"{request.Method} - {guid}", request.Path, body, logEventLevel);
         }
 
-        public static void AddLog(string guid, HttpResponse response, string body)
+        public static void AddLog(string guid, HttpResponse response, string body, LogEventLevel logEventLevel = LogEventLevel.Information)
         {
-            AddLog($"{response.StatusCode} - {guid}", string.Empty, body);
+            AddLog($"{response.StatusCode} - {guid}", string.Empty, body, logEventLevel);
+        }
+
+        private static void RegisterLog(LogEventLevel logEventLevel, string logInfo)
+        {
+            switch (logEventLevel)
+            {
+                case LogEventLevel.Verbose:
+                    Log.Verbose(logInfo);
+                    break;
+                case LogEventLevel.Debug:
+                    Log.Debug(logInfo);
+                    break;
+                case LogEventLevel.Warning:
+                    Log.Warning(logInfo);
+                    break;
+                case LogEventLevel.Error:
+                    Log.Error(logInfo);
+                    break;
+                case LogEventLevel.Fatal:
+                    Log.Fatal(logInfo);
+                    break;
+                default:
+                    Log.Information(logInfo);
+                    break;
+            }
         }
     }
 }
