f87c86b144ddd63d8273d72a4bdfbdd1a96d551e
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-01-24 16:28:10 -0300

    Create IMongoDbItem.cs

---
+++ FinCtrl/FinCtrlLibrary/Interfaces/IMongoDbItem.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FinCtrlLibrary.Interfaces
+{
+    public interface IMongoDbItem
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+    }
+}
