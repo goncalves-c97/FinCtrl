10e7dbb4b2c8cb475b1f8af0eab8c6bee7221b49
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-01-27 09:09:59 -0300

    SpendingRecord dedicated repository
    - Needs better alternative ratter than passing multiplies interfaces as parameters.

---
+++ FinCtrl/FinCtrlApi/Databases/MongoDb/FinCtrl/Interfaces/ISpendingRecordRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinCtrlApi.Databases.MongoDb.FinCtrl.Repositories;
+using FinCtrlLibrary.Models;
+
+namespace FinCtrlApi.Databases.MongoDb.FinCtrl.Interfaces
+{
+    public interface ISpendingRecordRepository : IGenericRepository<SpendingRecord>
+    {
+        public Task<List<SpendingRecord>> GetListWithIncludesAsync(IGenericRepository<SpendingCategory> spendingCategoryRepo, IGenericRepository<PaymentCategory> paymentCategoryRepo, IGenericRepository<TagCategory> tagCategoryRepo, IGenericRepository<SpendingRule> spendingRuleRepo);
+    }
+}
---
+++ FinCtrl/FinCtrlApi/Databases/MongoDb/FinCtrl/Repositories/SpendingRecordRepository.cs
@@ -0,0 +1,35 @@
+﻿using FinCtrlApi.Databases.MongoDb.FinCtrl.Interfaces;
+using FinCtrlLibrary.Models;
+
+namespace FinCtrlApi.Databases.MongoDb.FinCtrl.Repositories
+{
+    public class SpendingRecordRepository : GenericRepository<SpendingRecord>, ISpendingRecordRepository
+    {
+        public SpendingRecordRepository(FinCtrlAppDbContext context) : base(context) { }
+
+        public async Task<List<SpendingRecord>> GetListWithIncludesAsync(IGenericRepository<SpendingCategory> spendingCategoryRepo, IGenericRepository<PaymentCategory> paymentCategoryRepo, IGenericRepository<TagCategory> tagCategoryRepo, IGenericRepository<SpendingRule> spendingRuleRepo)
+        {
+            List<SpendingRecord> spendingRecords = await GetListAsync();
+
+            List<SpendingCategory> spendingCategories = await spendingCategoryRepo.GetListAsync();
+            List<PaymentCategory> paymentCategories = await paymentCategoryRepo.GetListAsync();
+            List<TagCategory> tagCategories = await tagCategoryRepo.GetListAsync();
+            List<SpendingRule> spendingRules = await spendingRuleRepo.GetListAsync();
+
+            foreach (SpendingRecord spendingRecord in spendingRecords)
+            {
+                SpendingCategory? spendingCategory = spendingCategories.FirstOrDefault(x => x._id.ToString() == spendingRecord.CategoryBsonId);
+                PaymentCategory? paymentCategory = paymentCategories.FirstOrDefault(x => x._id.ToString() == spendingRecord.PaymentCategoryBsonId);
+                List<TagCategory>? tagCategoriesList = tagCategories.Where(x => spendingRecord.Tags.Select(x => x._id.ToString()).ToList().Contains(x._id.ToString())).ToList();
+                SpendingRule? spendingRule = spendingRules.FirstOrDefault(x => x._id.ToString() == spendingRecord.SpendingRuleBsonId);
+
+                spendingRecord.Category = spendingCategory;
+                spendingRecord.PaymentCategory = paymentCategory;
+                spendingRecord.Tags = tagCategoriesList;
+                spendingRecord.SpendingRule = spendingRule;
+            }
+
+            return spendingRecords;
+        }
+    }
+}
