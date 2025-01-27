9e75d76ea5ff1e533c6317c7f55ce599f2195704
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-01-27 18:25:45 -0300

    New models

---
+++ FinCtrl/FinCtrlLibrary/Models/EarningCategory.cs
@@ -0,0 +1,17 @@
+﻿using FinCtrlLibrary.Models.GenericModels;
+
+namespace FinCtrlLibrary.Models
+{
+    public class EarningCategory : Category
+    {
+        public EarningCategory()
+        {
+
+        }
+
+        public EarningCategory(string bsonId, string name) : base(bsonId, name)  
+        {
+
+        }
+    }
+}
---
+++ FinCtrl/FinCtrlLibrary/Models/SalaryHistory.cs
@@ -0,0 +1,45 @@
+﻿using FinCtrlLibrary.Interfaces;
+using FinCtrlLibrary.Validators;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FinCtrlLibrary.Models
+{
+    public class SalaryHistory : ValidatorClass, IMongoDbItem
+    {
+        [BsonId]
+        public ObjectId _id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public double Salary { get; set; }
+
+        public SalaryHistory()
+        {
+            Validate();
+        }
+
+        public SalaryHistory(DateTime startDate, double salary)
+        {
+            StartDate = startDate;
+            EndDate = null;
+            Salary = salary;
+            Validate();
+        }
+
+        public SalaryHistory(DateTime startDate, DateTime endDate, double salary)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+            Salary = salary;
+            Validate();
+        }
+
+        protected override void Validate()
+        {
+            if (EndDate != null)
+                StartEndDateTimeValidation(StartDate, nameof(StartDate), (DateTime)EndDate, nameof(EndDate), true);
+
+            PositiveValueValidation(nameof(Salary), Salary, true);
+        }
+    }
+}
