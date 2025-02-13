3c065b5d1571333a259f9c7a93ea1f7e1556caf3
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-02-13 12:31:48 -0300

    Add new tests

---
+++ FinCtrl/FinCtrl.Test/Models/Constructors/EarningRecordConstructor.cs
@@ -0,0 +1,44 @@
+﻿using FinCtrlLibrary.Models;
+using FinCtrlLibrary.Validators;
+
+namespace FinCtrl.Test.Models.Constructors
+{
+    public class EarningRecordConstructor
+    {
+        [Fact]
+        public void ReturnsValidEarningRecordWhenDataIsValid()
+        {
+            DateTime dateTime = DateTime.Now;
+            string earningCategoryBsonId = "64c91f5e2b8f4e34a1d2cabc";
+            double value = 100;
+            bool validation = true;
+
+            EarningRecord earningRecord = new(dateTime, earningCategoryBsonId, value);
+
+            Assert.Equal(validation, earningRecord.IsValid);
+        }
+
+        [Fact]
+        public void ReturnsValidEarningRecordWhenDateTimeIsNotInformed()
+        {
+            string earningCategoryBsonId = "64c91f5e2b8f4e34a1d2cabc";
+            double value = 100;
+            bool validation = true;
+
+            EarningRecord earningRecord = new(earningCategoryBsonId, value);
+
+            Assert.Equal(validation, earningRecord.IsValid);
+        }
+
+        [Theory]
+        [InlineData("InvalidBsonId", 100, GenericErrors.InvalidBsonIdError, nameof(EarningRecord.EarningCategoryBsonId), false)]
+        [InlineData("64c91f5e2b8f4e34a1d2cabc", 0, GenericErrors.ValueZeroError, nameof(EarningRecord.Value), false)]
+        [InlineData("64c91f5e2b8f4e34a1d2cabc", -100, GenericErrors.NegativeValueError, nameof(EarningRecord.Value), false)]
+        public void ReturnsErrorWhenPossibleValuesAreInvalid(string earningCategoryBsonId, double value, Enum error, string propertyName, bool validation)
+        {
+            EarningRecord earningRecord = new(earningCategoryBsonId, value);
+            Assert.Equal(earningRecord.IsValid, validation);
+            Assert.True(earningRecord.ContainsError(error, propertyName));
+        }
+    }
+}
---
+++ FinCtrl/FinCtrl.Test/Models/Constructors/SalaryHistoryConstructor.cs
@@ -0,0 +1,61 @@
+﻿using FinCtrlLibrary.Models;
+using FinCtrlLibrary.Utilities;
+using FinCtrlLibrary.Validators;
+using Microsoft.VisualStudio.TestPlatform.Common.DataCollection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinCtrl.Test.Models.Constructors
+{
+    public class SalaryHistoryConstructor
+    {
+        [Theory]
+        [InlineData("2010-01-01", "2024-12-31", 10000, true)]
+        [InlineData("2024-12-01", "2024-12-31", 10000, true)]
+        [InlineData("2024-12-01 00:00:00", "2024-12-01 23:59:59", 10000, true)]
+        public void ReturnsValidSalaryHistoryWhenDataIsValid(DateTime startDateTime, DateTime endDateTime, double salary, bool validation)
+        {
+            SalaryHistory salaryHistory = new(startDateTime, endDateTime, salary);
+            Assert.Equal(salaryHistory.IsValid, validation);
+        }
+
+        [Fact]
+        public void ReturnsValidSalaryHistoryWhenNotSpecifyingEndDate()
+        {
+            DateTime startDateTime = new(2010, 01, 01);
+            double salary = 10000;
+            bool validation = true;
+
+            SalaryHistory salaryHistory = new(startDateTime, salary);
+            Assert.Equal(salaryHistory.IsValid, validation);
+        }
+
+        [Theory]
+        [InlineData("2010-01-01", "2024-12-31", 0, false)]
+        [InlineData("2010-01-01", "2024-12-31", -10000, false)]
+        [InlineData("2010-01-01", "2010-01-01", 10000, false)]
+        [InlineData("2024-12-31", "2024-12-01", 10000, false)]
+        [InlineData("2024-12-01 23:59:59", "2024-12-01 00:00:00", 10000, false)]
+        public void ReturnsInvalidSalaryHistoryWhenDataIsInvalid(DateTime startDateTime, DateTime endDateTime, double salary, bool validation)
+        {
+            SalaryHistory salaryHistory = new(startDateTime, endDateTime, salary);
+            Assert.Equal(salaryHistory.IsValid, validation);
+        }
+
+        [Theory]
+        [InlineData("2010-01-01", "2024-12-31", 0, GenericErrors.ValueZeroError, new string [] { nameof(SalaryHistory.Salary) }, false)]
+        [InlineData("2010-01-01", "2024-12-31", -10000, GenericErrors.NegativeValueError, new string[] { nameof(SalaryHistory.Salary) }, false)]
+        [InlineData("2010-01-01", "2010-01-01", 10000, GenericErrors.SameDateTimeError, new string[] { nameof(SalaryHistory.StartDate), nameof(SalaryHistory.EndDate) }, false)]
+        [InlineData("2024-12-31", "2024-12-01", 10000, GenericErrors.StartBiggerThanEndDateTimeError, new string[] { nameof(SalaryHistory.StartDate), nameof(SalaryHistory.EndDate) }, false)]
+        [InlineData("2024-12-01 23:59:59", "2024-12-01 00:00:00", 10000, GenericErrors.StartBiggerThanEndDateTimeError, new string[] { nameof(SalaryHistory.StartDate), nameof(SalaryHistory.EndDate) }, false)]
+        public void ReturnsErrorWhenPossibleValuesAreInvalid(DateTime startDateTime, DateTime endDateTime, double salary, Enum error, string [] propertyNames, bool validation)
+        {
+            SalaryHistory salaryHistory = new SalaryHistory(startDateTime, endDateTime, salary);
+            Assert.Equal(salaryHistory.IsValid, validation);
+            Assert.True(salaryHistory.ContainsError(error, propertyNames));
+        }
+    }
+}
