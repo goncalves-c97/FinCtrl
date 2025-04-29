91e12d14b11731c1659db94e3f73f0c83dc2634b
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-04-29 15:45:45 -0300

    Create AppSettingsValueNotFoundException.cs
    - Added new exception for not found appsettings value.

---
+++ FinCtrl/FinCtrlLibrary/Exceptions/AppSettingsValueNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace FinCtrlLibrary.Exceptions
+{
+    public class AppSettingsValueNotFoundException : Exception
+    {
+        public AppSettingsValueNotFoundException() : base() { }
+        public AppSettingsValueNotFoundException(string message) : base(message) { }
+        public AppSettingsValueNotFoundException(string message, Exception e) : base(message, e) { }
+    }
+}
