ff75e233fcca43aa0d4a6f33f45d57c62f2133e6
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-04-29 15:44:13 -0300

    Create UserRoles.cs

---
+++ FinCtrl/FinCtrlLibrary/Models/StaticModels/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace FinCtrlLibrary.Models.StaticModels
+{
+    public static class UserRoles
+    {
+        public const string Admin = "1";
+        public const string CommonUser = "2";
+    }
+}
