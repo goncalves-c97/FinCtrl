02c25838f975eb10b3312aa43b2dcef712ef0d21
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-04-29 15:47:07 -0300

    Update Program.cs
    - Added authentication stuff + AuthenticationRepository and UserRepository stuff

--- FinCtrl/FinCtrlApi/Program.cs
+++ FinCtrl/FinCtrlApi/Program.cs
@@ -2,11 +2,16 @@ using FinCtrlApi.Databases.MongoDb.FinCtrl;
 using FinCtrlApi.Databases.MongoDb.FinCtrl.Interfaces;
 using FinCtrlApi.Databases.MongoDb.FinCtrl.Repositories;
 using FinCtrlApi.Entities;
+using FinCtrlApi.Services;
+using FinCtrlLibrary.Exceptions;
 using FinCtrlLibrary.Middlewares;
 using FinCtrlLibrary.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Bson.Serialization.Conventions;
 using Serilog;
+using System.Text;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -15,6 +20,8 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Database Configuration
 
 ConfigureMongoDB();
 
@@ -30,10 +37,32 @@ if (mongoDbConnectionString == null)
 builder.Services.AddDbContextPool<FinCtrlAppDbContext>(
     options => options.UseMongoDB(mongoDbConnectionString, "finctrl_dtb"));
 
+#endregion
+
+#region TokenService Configuration
+
+string? tokenSecret = builder.Configuration.GetValue<string>("TokenServiceSecret");
+
+if (string.IsNullOrEmpty(tokenSecret))
+    throw new AppSettingsValueNotFoundException("'TokenServiceSecret' não encontrado");
+
+TokenService.SetSecret(tokenSecret);
+
+int? tokenHoursToExpire = builder.Configuration.GetValue<int?>("TokenServiceHoursToExpire");
+
+if(tokenHoursToExpire == null)
+    throw new AppSettingsValueNotFoundException("'TokenServiceHoursToExpire' não encontrado");
+
+TokenService.SetHoursToExpire((int)tokenHoursToExpire);
+
+#endregion
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Interfaces/Repositories Configuration
 
 builder.Services.AddScoped<IGenericRepository<SpendingCategory>, GenericRepository<SpendingCategory>>();
 builder.Services.AddScoped<IGenericRepository<PaymentCategory>, GenericRepository<PaymentCategory>>();
@@ -42,7 +71,30 @@ builder.Services.AddScoped<IGenericRepository<SpendingRule>, GenericRepository<S
 builder.Services.AddScoped<IGenericRepository<EarningCategory>, GenericRepository<EarningCategory>>();
 builder.Services.AddScoped<ISpendingRecordRepository, SpendingRecordRepository>();
 builder.Services.AddScoped<IEarningRecordRepository, EarningRecordRepository>();
+builder.Services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+#endregion
+
+byte[] key = Encoding.ASCII.GetBytes(tokenSecret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
