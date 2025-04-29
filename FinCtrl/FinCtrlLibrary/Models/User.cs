86bb2efba26565075e539f8138bc6f565b58c17b
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-04-29 15:44:51 -0300

    Update User.cs
    - Added new properties and validation

--- FinCtrl/FinCtrlLibrary/Models/User.cs
+++ FinCtrl/FinCtrlLibrary/Models/User.cs
@@ -1,15 +1,32 @@
 ﻿using FinCtrlLibrary.Interfaces;
+using FinCtrlLibrary.Validators;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace FinCtrlLibrary.Models
 {
-    public class User : IMongoDbItem
+    public class User : ValidatorClass, IMongoDbItem
     {
+        [BsonId]
         public ObjectId _id { get; set; }
-        public string UserName { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime CreationDateTime { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime LastLogin { get; set; }
+
+        protected override void Validate()
+        {
+            ObjectIdValidation(nameof(_id), _id.ToString());
+            NotEmptyStringValidation(nameof(Name), Name);
+            NotEmptyStringValidation(nameof(Email), Email);
+            StartEndDateTimeValidation(CreationDateTime, nameof(CreationDateTime), DateTime.Now, nameof(DateTime.Now));
+            NotEmptyStringValidation(nameof(Password), Password);
+            NotEmptyStringValidation(nameof(Role), Role);
+            NotBiggerThanNowDateTimeValidation(nameof(DateOfBirth), DateOfBirth);
+            NotBiggerThanNowDateTimeValidation(nameof(LastLogin), LastLogin);
+        }
     }
 }
