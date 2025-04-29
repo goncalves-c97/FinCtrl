3bb435f0b4eee035ace7c98471141616f25b3b1a
Carlos Henrique <carlos_henrique97@outlook.com.br>
2025-04-29 15:48:37 -0300

    Create UserController.cs

---
+++ FinCtrl/FinCtrlApi/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using FinCtrlApi.Databases.MongoDb.FinCtrl.Interfaces;
+using FinCtrlLibrary.Models.StaticModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FinCtrlApi.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet, Route("GetUsersList"), Authorize(Roles = UserRoles.Admin)]
+        public async Task<IActionResult> GetUsersListAsync()
+        {
+            try
+            {
+                return Ok(await _userRepo.GetListAsync());
+            }
+            catch(Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
